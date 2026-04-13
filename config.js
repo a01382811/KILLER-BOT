856fad83bbdf92ca7bb1b799d6a3b05adea0a925
a01382811 <a01382811@gmail.com>
2026-04-13 23:03:10 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    prefix: '.',
+    botName: 'KILLER-MINI-BOT',
+    author: 'KILLER',
+    ownerNumber: '22784566540@s.whatsapp.net',
+    ownerName: 'KILLER',
+    version: '3.0'
+};
